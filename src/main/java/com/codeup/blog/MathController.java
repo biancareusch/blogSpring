9b1e8a7d0322754506cbc3ac32704c1462ee0863
Bianca Reusch <reusch.bianca@gmail.com>
2020-11-30 10:38:39 -0600

    added partials for head, navbar and footer

--- src/main/java/com/codeup/blog/MathController.java
+++ src/main/java/com/codeup/blog/MathController.java
@@ -17,12 +17,12 @@ public class MathController {
     public String subtract(@PathVariable int num1, @PathVariable int num2){
         return (num1 + " - " + num2 + " = " + (num1 - num2));
     }
-    @GetMapping("/divide/{num1}/and/{num2}")
+    @GetMapping("/divide/{num1}/by/{num2}")
     @ResponseBody
     public String divide(@PathVariable int num1, @PathVariable int num2){
         return (num1 + " / " + num2 + " = " + (num1 / num2));
     }
-    @GetMapping("/multiply/{num1}/by/{num2}")
+    @GetMapping("/multiply/{num1}/and/{num2}")
     @ResponseBody
     public String multiply(@PathVariable int num1, @PathVariable int num2){
         return (num1 + " * " + num2 + " = " + (num1 * num2));
