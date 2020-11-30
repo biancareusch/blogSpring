01c7b26ff8c56599bbd897c170afab009faa696f
Bianca Reusch <reusch.bianca@gmail.com>
2020-11-30 11:10:34 -0600

    controller displaying posts

--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -1,21 +1,34 @@
 package com.codeup.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
-    @GetMapping("/posts")
+    @GetMapping("/posts/index")
     @ResponseBody
-    public String indexPage(){
-        return "posts index page";
+    public List<Post> showPosts(Model model){
+        List<Post> allPosts = new ArrayList<>();
+        Post firstPost = new Post(1,"First Post","Hello World!");
+        Post secondPost = new Post(2,"new Post", "what's going on");
+        allPosts.add(firstPost);
+        allPosts.add(secondPost);
+        model.addAttribute("allPosts", allPosts);
+        return allPosts;
     }
-    @GetMapping("/posts/{id}")
+    @GetMapping("/posts/show/{id}")
     @ResponseBody
-    public String individualPost(){
-        return "view an individual post";
+    public Post individualPost(@PathVariable long id){
+//        Post thisPost = Post.getPostByID(id);
+        Post thisPost = new Post(id,"show Post","showing individual post");
+        return thisPost;
     }
     @GetMapping("/posts/create")
     @ResponseBody
